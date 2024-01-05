ac2b293811c58ccac728675febc82493f42d8553
Paulogsiqueira <pgustavo-siqueira@hotmail.com>
2024-01-05 17:26:53 -0300

    Drag and Drop

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -15,7 +15,8 @@ const Dashboard = () => {
     { id: "12432", title: "Quinta Tarefa" },
     { id: "12", title: "Sexta Tarefa" },
     { id: "123", title: "Setima Tarefa" },
-    { id: "1234", title: "Oitava Tarefa" }
+    { id: "1234", title: "Oitava Tarefa" },
+    { id: "12345", title: "Nona Tarefa" }
   ];
 
 
