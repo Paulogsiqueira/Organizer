257f5f4a8bcaf2ecbc79ec2937fdc1add3ac1b66
Paulogsiqueira <pgustavo-siqueira@hotmail.com>
2024-02-01 11:29:27 -0300

    Corrigndo routes

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectUser } from '@/redux/sliceUser'
+
 import Register from '../pages/register/Register'
 import Home from '../pages/home/Home'
 import Login from '../pages/login/Login'
@@ -9,8 +8,7 @@ import Dashboard from '@/pages/dashboard/Dashboard'
 import Statistics from '@/pages/statistics/Statistics'
 
 const PageRoutes = () => {
-    const user = useSelector(selectUser)
-    const userLogged: boolean = (user.isLogged)
+
 
     return (
         <Routes>
