f421d0f8aa89ec7f85a10b736b1b856bb7e1228b
Paulogsiqueira <pgustavo-siqueira@hotmail.com>
2024-02-01 11:23:57 -0300

    Corrigndo rotas

--- src/routes/PageRoutes.tsx
+++ src/routes/PageRoutes.tsx
@@ -1,27 +1,40 @@
-import { Routes, Route } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { selectUser } from '@/redux/sliceUser'
-import Register from '../pages/register/Register'
-import Home from '../pages/home/Home'
-import Login from '../pages/login/Login'
-import Perfil from '@/pages/perfil/Perfil'
-import Dashboard from '@/pages/dashboard/Dashboard'
-import Statistics from '@/pages/statistics/Statistics'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUser } from '@/redux/sliceUser';
+import Register from '../pages/register/Register';
+import Home from '../pages/home/Home';
+import Login from '../pages/login/Login';
+import Perfil from '@/pages/perfil/Perfil';
+import Dashboard from '@/pages/dashboard/Dashboard';
+import Statistics from '@/pages/statistics/Statistics';
 
 const PageRoutes = () => {
-    const user = useSelector(selectUser)
-    const userLogged: boolean = (user.isLogged)
+    const user = useSelector(selectUser);
+    const userLogged = user.isLogged;
+
+    if (!userLogged) {
+        return (
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/dashboard' element={<Navigate to="/" />} />
+                <Route path='/statistics' element={<Navigate to="/" />} />
+                <Route path='/perfil' element={<Navigate to="/" />} />
+            </Routes>
+        );
+    }
 
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/register' element={ !userLogged ? <Register /> : <Home/>} />
-            <Route path='/login' element={ !userLogged ? <Login /> : <Home/>} />
-            <Route path='/dashboard' element={ userLogged ? <Dashboard /> : <Home/>} />
-            <Route path='/statistics' element={ userLogged ? <Statistics /> : <Home/>} />
-            <Route path='/perfil' element={ userLogged ? <Perfil /> : <Home/>} />
+            <Route path='/register' element={<Navigate to="/" />} />
+            <Route path='/login' element={<Navigate to="/" />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/statistics' element={<Statistics />} />
+            <Route path='/perfil' element={<Perfil />} />
         </Routes>
-    )
-}
+    );
+};
 
-export default PageRoutes
+export default PageRoutes;
